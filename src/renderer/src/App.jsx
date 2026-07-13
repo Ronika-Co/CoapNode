349a7b8192d3c59c18195a255791b189fed44d60
sobhan.nami <sobhan.nami76@gmail.com>
2026-07-13 15:24:11 +0330

    0.0.4

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1599,7 +1599,7 @@ export default function App() {
             </div>
 
             <div className="p-4 border-t border-white/5 text-xs text-slate-500 text-center">
-              CoapNode v0.0.3 &bull;
+              CoapNode v0.0.4 &bull;
             </div>
           </aside>
 
